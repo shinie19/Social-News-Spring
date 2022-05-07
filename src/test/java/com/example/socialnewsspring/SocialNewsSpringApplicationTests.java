3680158fc128b129768b42b498d7c3a438bee92a
nguyenanhhus <nguyenanh.binoo@gmail.com>
2022-05-07 16:04:46 +0700

    Initial project

---
+++ src/test/java/com/example/socialnewsspring/SocialNewsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialnewsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNewsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
