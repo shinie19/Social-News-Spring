4b11e469b26aa29098a9629f45eaa1dae06a9798
nguyenanhhus <nguyenanh.binoo@gmail.com>
2022-05-15 01:02:08 +0700

    Create document API Swagger

--- src/main/java/com/example/socialnewsspring/SocialNewsSpringApplication.java
+++ src/main/java/com/example/socialnewsspring/SocialNewsSpringApplication.java
@@ -1,11 +1,16 @@
 package com.example.socialnewsspring;
 
+import com.example.socialnewsspring.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
+@EnableWebMvc
 public class SocialNewsSpringApplication {
 
     public static void main(String[] args) {
